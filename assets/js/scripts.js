78893c20dcbfb51c1141ecdee5b30d76fa57e42b
Fernando Vargas <fvargaspf@gmail.com>
2020-04-02 08:57:53 -0300

    Criacao de modais e ajustes finos desta etapa do Foodfy

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,15 +4,16 @@ const cards = document.querySelectorAll('.card');
 // exibir o modal
 for (let card of cards) {
     card.addEventListener('click', function(){
-        const videoId = card.getAttribute('id');
-
         modalOverlay.classList.add('active');
-        modalOverlay.querySelector("iframe").src = `https://www.youtube.com/embed/${videoId}`;
+		modalOverlay.querySelector("img").src = card.querySelector("img").src;
+		modalOverlay.querySelector("img").alt = card.querySelector("img").alt;
+		modalOverlay.querySelector("p").innerHTML = card.querySelector("p").innerHTML;
+		modalOverlay.querySelector("legend").innerHTML = card.querySelector("legend").innerHTML;
     });
 }
 
 // ocultar o modal
 document.querySelector('.close-modal').addEventListener('click', function(){
     modalOverlay.classList.remove('active');
-    modalOverlay.querySelector("iframe").src = '';
+    modalOverlay.querySelector("img").src = '';
 });
