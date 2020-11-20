300e734f15a6ca497c4cb0f2007a377e7837a2cf
Fernando Vargas <fvargaspf@gmail.com>
2020-11-20 17:50:20 -0300

    ci: :sparkles: [Rotas]: adicionado rotas para modulo administrativo de receitas

--- routes.js
+++ routes.js
@@ -1,6 +1,7 @@
 const express = require('express'),
 	  routes = express.Router(),
-	  receitas = require('./controllers/receitas')
+	  receitas = require('./controllers/receitas'),
+	  recipes = require('./controllers/admin/recipes')
 ;
 
 routes.get('/', receitas.index);
@@ -10,5 +11,15 @@ routes.get('/receitas/:index', receitas.show);
 routes.get('/sobre', function(req,res) {
 	return res.render('sobre');
 });
+
+// Área administrativa
+routes.get("/admin/recipes", recipes.index); // Mostrar a lista de receitas
+routes.get("/admin/recipes/create", recipes.create); // Mostrar formulário de nova receita
+routes.get("/admin/recipes/:id", recipes.show); // Exibir detalhes de uma receita
+routes.get("/admin/recipes/:id/edit", recipes.edit); // Mostrar formulário de edição de receita
+
+routes.post("/admin/recipes", recipes.post); // Cadastrar nova receita
+routes.put("/admin/recipes", recipes.put); // Editar uma receita
+routes.delete("/admin/recipes", recipes.delete); // Deletar uma receita
 
 module.exports = routes;
